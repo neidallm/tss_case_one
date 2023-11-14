0128284ecf8ee8db57446163afdef800dfc69534
neidallm <neida.llusco1999@gmail.com>
2023-11-13 23:13:55 -0400

    tabla_funciones

--- src/components/TablaResultados/TablaRes.js
+++ src/components/TablaResultados/TablaRes.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import {tablaNroTrabajador,tablaAcumulada,factorial} from "./tablas";
 
-
+const tablitaRes = [
+  {rand:0,cantCamiones:0,tiempoEsperaTotal:0,costoEspera:0},
+  {rand:0,cantCamiones:0,tiempoEsperaTotal:0,costoEspera:0},
+  {rand:0,cantCamiones:0,tiempoEsperaTotal:0,costoEspera:0},
+  {rand:0,cantCamiones:0,tiempoEsperaTotal:0,costoEspera:0},
+  {rand:0,cantCamiones:0,tiempoEsperaTotal:0,costoEspera:0},
+  {rand:0,cantCamiones:0,tiempoEsperaTotal:0,costoEspera:0},
+  {rand:0,cantCamiones:0,tiempoEsperaTotal:0,costoEspera:0},
+]
 
 const TablaRes = () => {
+
+
   // Obtener datos del localStorage
  const datosLocalStorage = JSON.parse(localStorage.getItem('datos')) || [];
- 
- console.log(datosLocalStorage);
-  // Condición para verificar si hay datos en el localStorage
  const hayDatos = datosLocalStorage.length > 0;
- console.log(hayDatos);
+
+tablaNroTrabajador.map((element)=>{
+  element.randon =  Math.floor(Math.random() * (element.max - element.min + 1)) + element.min ;
+});
+
+
+tablitaRes.map((element) => {
+  element.rand = Math.random();
+
+  //console.log(tablaAcumulada.find((elem)=>element.rand >= elem.limInf && element.rand <= elem.limSup));
+  element.cantCamiones = tablaAcumulada.find((elem)=>element.rand >= elem.limInf && element.rand <= elem.limSup).nro;
+
+  //console.log(tablaNroTrabajador.find((nroT)=>nroT.nro === datosLocalStorage[0].nroTrabajadores));
+  element.tiempoEsperaTotal = element.cantCamiones * tablaNroTrabajador.find((nroT)=>nroT.nro === datosLocalStorage[0].nroTrabajadores).randon;
+  element.costoEspera = element.tiempoEsperaTotal * (datosLocalStorage[0].costoEspera / 60) 
+  
+});
+
+console.log(tablitaRes);
+
 
   return (
     <div className='container'>
@@ -19,18 +46,26 @@ const TablaRes = () => {
           <thead>
             <tr>
               <th scope="col">Horas</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Aleatorio</th>
+              <th scope="col">Cantidad de Camiones</th>
+              <th scope="col">Número de trabajadores</th>
+              <th scope="col">Sueldo Trabajador</th>
+              <th scope="col">Tiempo de espera minutos</th>
+              <th scope="col">Tiempo de espera Total</th>
+              <th scope="col">Costo de espera</th>
+              <th scope="col">Costo Total</th>
             </tr>
           </thead>
           <tbody>
-            {/* Lógica para renderizar filas de la tabla según los datos del localStorage */}
-            {datosLocalStorage.map((dato, index) => (
+            {tablitaRes.map((dato,index) => (
               <tr key={index}>
-                <th scope="row">{dato.horas}</th>
-                <td>{dato.first}</td>
-                <td>{dato.last}</td>
+                <th>{index+1}</th>
+                <td>{dato.rand}</td>
+                <td>{dato.cantCamiones}</td>
+                <td>{datosLocalStorage[0].nroTrabajadores}</td>
+                <td>{datosLocalStorage[0].sueldo}</td>
+                <td>{dato.tiempoEsperaTotal}</td>
+                <td>{dato.handle}</td>
                 <td>{dato.handle}</td>
               </tr>
             ))}
--- src/components/TablaResultados/tablas.js
+++ src/components/TablaResultados/tablas.js
@@ -1,5 +1,5 @@
 
- const tablaNroTrabajadore = [{
+ const tablaNroTrabajador = [{
     nro : 2,
     min: 25,
     max: 35,
@@ -32,19 +32,27 @@
 const tablaAcumulada = [{
     nro:0,
     limInf:0,
-    limSup:0
+    limSup:0.15
     },
     {nro:1,
-    limInf:0,
-    limSup:0
+    limInf:0.15,
+    limSup:0.4
     },
     {nro:2,
-    limInf:0,
-    limSup:0
+    limInf:0.4,
+    limSup:0.9
     },
     {nro:3,
-    limInf:0,
-    limSup:0
-    }]
+    limInf:0.9,
+    limSup:1
+    }];
 
-export  {tablaNroTrabajadore, tablaAcumulada };
+export    function factorial(n) {
+        if (n === 0 || n === 1) {
+          return 1;
+        } else {
+          return n * factorial(n - 1);
+        }
+      }
+
+export  {tablaNroTrabajador, tablaAcumulada };
